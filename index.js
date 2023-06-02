ced0ad47c44afdf6a3fd0e01cf8ad0d871b0ad33
Rashed967 <rashedhasan967@gmail.com>
2023-06-02 23:48:56 +0600

    added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express()
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -21,6 +22,21 @@ const client = new MongoClient(uri, {
   }
 });
 
+const verifyJwt = (req, res, next) => {
+  const authorization = req.headers.authorization
+  if(!authorization){
+    return res.status(403).send({error : true, message : "UnAuthorized access"})
+  }
+  const token = authorization.split(' ')[1]
+  verifyJwt(token, process.env.ACCESS_TOKEN, (error, decoded) => {
+    if(error){
+      return res.status(402).send({error : true, message : "Bad gathway"})
+    }
+    req.decoded = decoded
+    next()
+  })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -32,10 +48,20 @@ async function run() {
     const cartCollection = client.db('bistroDB').collection('carts')
 
 
+    // jwt api 
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      // console.log(body)
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn : "10h"
+      })
+      res.send({token})
+    })
+
 
     // user api 
     
-    app.get('/users', async(req, res) => {
+    app.get('/users', verifyJwt, async(req, res) => {
       const result = await userCollection.find().toArray()
       res.send(result)
     })
@@ -59,7 +85,7 @@ async function run() {
 
 
     // reviews collection 
-    app.get("/reviews", async (req, res) => {
+    app.get("/reviews",  async (req, res) => {
         const result = await reviewCollection.find().toArray()
         res.send(result)
     })
@@ -67,11 +93,15 @@ async function run() {
 
     // cart collection 
 
-    app.get('/carts', async(req, res) => {
+    app.get('/carts',verifyJwt,  async(req, res) => {
       const email = req.query.email
       if(!email){
         res.send([])
       }
+      // const decodedEmail = req.decoded.email
+      // if(email !== decodedEmail){
+      //   return res.status(401).send({error : true, message :" invalid email address"})
+      // }
       const query = {email : email}
       const result = await cartCollection.find(query).toArray()
       res.send(result)
@@ -84,6 +114,19 @@ async function run() {
       res.send(result)
     })
 
+
+    app.patch('/users/admin/:id', async(req, res) => {
+      const id = req.params.id
+      console.log(id)
+      const filter = {_id : new ObjectId(id)}
+      const updateDoc = {
+        $set : {
+          role : 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter, updateDoc)
+      res.send(result)
+    })
 
     app.delete('/carts/:id', async(req, res) => {
       const id = req.params.id
